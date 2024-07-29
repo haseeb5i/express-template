9fc2b67b89b1c7e1a17b5855a687b25a1f38f25b
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-07-29 11:58:30 +0500

    chore: format stuff

--- src/api-docs/__tests__/openAPIRouter.test.ts
+++ src/api-docs/__tests__/openAPIRouter.test.ts
@@ -1,22 +1,22 @@
-import { StatusCodes } from 'http-status-codes';
-import request from 'supertest';
+import { StatusCodes } from "http-status-codes";
+import request from "supertest";
 
-import { app } from '@/server';
+import { app } from "@/server";
 
-import { generateOpenAPIDocument } from '../openAPIDocumentGenerator';
+import { generateOpenAPIDocument } from "../openAPIDocumentGenerator";
 
-describe('OpenAPI Router', () => {
-  describe('Swagger JSON route', () => {
-    it('should return Swagger JSON content', async () => {
+describe("OpenAPI Router", () => {
+  describe("Swagger JSON route", () => {
+    it("should return Swagger JSON content", async () => {
       // Arrange
       const expectedResponse = generateOpenAPIDocument();
 
       // Act
-      const response = await request(app).get('/swagger.json');
+      const response = await request(app).get("/swagger.json");
 
       // Assert
       expect(response.status).toBe(StatusCodes.OK);
-      expect(response.type).toBe('application/json');
+      expect(response.type).toBe("application/json");
       expect(response.body).toEqual(expectedResponse);
     });
 
